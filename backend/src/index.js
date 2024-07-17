16c7e37d3184c6e6c27a887b7a79ca39137178c6
Noam Avitan <noamavitan159753@gmail.com>
2024-07-17 16:45:24 +0300

    cors edit

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 3003;
 const MONGO_URI = process.env.MONGO_URI;
 
 //middlewares
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use((req, res, next) => {
